9ee11cae0d38cbfcd8d99e26d0f951475f8e40ad
Koki Tsumura <koki827tt@gmail.com>
2020-05-17 09:13:28 +0900

    updated snippet

--- vim-templates/cpp/base-main.cpp
+++ vim-templates/cpp/base-main.cpp
@@ -1,35 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
+
+typedef long long      ll;
+typedef vector<int>    vi;
 typedef vector<string> vs;
+
 #define FOR(i, a, b) for (int i = (a); i < (b); i++)
-#define rep(i, n) FOR(i, 0, n)
-#define pb push_back
-#define all(a) (a).begin(), (a).end()
-#define UNIQUE(v) v.erase(unique(all(v)), v.end());
-#define fill(a, x) memset(a, x, sizeof(a))
-#define zero(a) fill(a, 0)
-#define minus(a) fill(a, -1)
-#define dbg(x) cout << #x "=" << x << endl
+#define rep(i, n)    FOR(i, 0, n)
+#define pb           push_back
+#define all(a)       (a).begin(), (a).end()
+#define UNIQUE(v)    v.erase(unique(all(v)), v.end());
+#define fill(a, x)   memset(a, x, sizeof(a))
+#define zero(a)      fill(a, 0)
+#define minus(a)     fill(a, -1)
+#define dbg(x)       cout << #x "=" << x << endl
 
 int main() {
+  /*入力を受け取るための処理 start*/
   int n;
+  int count = 0;
+  vi  a;
   cin >> n;
+
+  /*extra start*/
   // 最初の入力がn kのように与えられている場合のみコメントアウトする
   // int k;
   // cin >> k;
   // res: 結果の出力用
   // int res;
-  int count = 0;
-  int tmp;
-  vi a;
+  /*extra end*/
+
   while (count < n) {
+    int tmp;
     cin >> tmp;
     a.pb(tmp);
     count++;
   }
+  /*入力を受け取るための処理 end*/
 
-  rep(i, n) cout << a[i] << endl;
+  rep(i, n) {
+    cout << a[i] << endl;
+  }
   return 0;
 }
