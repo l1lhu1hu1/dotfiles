a0d5ee6380cc80073a21ea99b082f541bb426afd
Koki Tsumura <koki827tt@gmail.com>
2020-06-04 15:48:52 +0900

    updated template

---
+++ library/huihuilib.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define rep(i, n)    FOR(i, 0, n)
+#define pb           push_back
+#define all(a)       (a).begin(), (a).end()
+#define UNIQUE(v)    v.erase(unique(all(v)), v.end());
+#define fill(a, x)   memset(a, x, sizeof(a))
+#define zero(a)      fill(a, 0)
+#define minus(a)     fill(a, -1)
+#define dbg(x)       cout << #x "=" << x << endl
+
+//#################################################
+// naming rules
+// bugをなるべく生み出さないために以下に従うこと
+// queue starts with "Q". for example QA
+// array starts with "A". for example AA
+// stack starts with "S". for example SA
+// vector starts with V. for example VA
+// global variable starts with "gl". for example glA
+// counter variable starts with "cnt". for example cntC
+
+//#################################################
+// string related
+int charToInt(char c) {
+  string tmp;
+  tmp += c;
+  int b = atoi(tmp.c_str());
+  return b;
+}
+
+int stringToInt(string s) {
+  return atoi(s.c_str());
+}
+//#################################################
+// vector related
+// deletes VT[from_index] through VT[to_index]
+// for example if from_index is 1 and to_index is 3, deletes VT[1] and VT[2]
+
+void delIndexBetween(vi VT, int fromIndex, int toIndex) {
+  VT.erase(VT.begin() + fromIndex, VT.begin() + toIndex);
+}
+
+void delIndex(vi VT, int index) {
+  VT.erase(VT.begin() + index);
+}
+
+void del(vi VT, int num) {
+  int id = -1;
+  rep(i, VT.size()) {
+    if (num == VT[i]) {
+      id = i;
+      break;
+    }
+  }
+  if (id != -1) {
+    delIndex(VT, id);
+  }
+}
+//#################################################
+// input
+// comment out below when input is given in the format n k
+/* n k */
+// int k;
+// cin >> n >> k
+//#################################################
+// output
+// for result output like below
+/*
+   p1 500
+   p2 1000
+   p3 1000
+*/
+
+// rep(i, n) {
+//   cout << C[i].name << " " << C[i].ms << endl;
+// }
--- vim-templates/cpp/base-main.cpp
+++ vim-templates/cpp/base-main.cpp
@@ -15,76 +15,20 @@ typedef vector<string> vs;
 #define minus(a)     fill(a, -1)
 #define dbg(x)       cout << #x "=" << x << endl
 
-// naming rules
-// array starts with "ar". for example arA
-// stack starts with "st". for example stA
-// queue starts with "qu". for example quA
-// vector starts with capital letter. for example A
-
-/* string related
-int charToInt(char c) {
-  string tmp;
-  tmp += c;
-  int b = atoi(tmp.c_str());
-  return b;
-}
-int stringToInt(string s) {
-  return atoi(s.c_str());
-}
-*/
-
-/* vector related
-// deletes T[from_index] through T[to_index]
-// for example if from_index is 1 and to_index is 3, deletes T[1], T[2]
-void delIndexBetween(vi T, int from_index, int to_index) {
-  T.erase(T.begin() + from_index, T.begin() + to_index);
-}
-void delIndex(vi T, int index) {
-  T.erase(T.begin() + index);
-}
-void del(vi T, int num) {
-  int id = -1;
-  rep(i, T.size()) {
-    if (num == T[i]) {
-      id = i;
-      break;
-    }
-  }
-  if (id != -1) delIndex(id);
-}
-*/
-
 int main() {
   ios::sync_with_stdio(false);
-  int c = 0;
-  vi A;
-
-  // comment out below when input is given in the format n k
-  /* n k */
-  // int k;
-  // cin >> n >> k
+  int cntC = 0;
+  vi VA;
 
   int n;
   cin >> n;
 
-  // receiving input
-  while (c < n) {
+  while (cntC < n) {
     int tmp;
     cin >> tmp;
-    A.pb(tmp);
-    c++;
+    VA.pb(tmp);
+    cntC++;
   }
-
-  // for result output like below
-  /*
-     p1 500
-     p2 1000
-     p3 1000
-  */
-
-  // rep(i, n) {
-  //   cout << C[i].name << " " << C[i].ms << endl;
-  // }
 
   return 0;
 }
