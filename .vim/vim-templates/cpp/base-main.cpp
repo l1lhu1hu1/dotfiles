c7be41707d4c5db9319c87a7fc338351fabc7352
Koki Tsumura <koki827tt@gmail.com>
2021-01-07 13:09:07 +0900

    updated vim snippet

--- .vim/vim-templates/cpp/base-main.cpp
+++ .vim/vim-templates/cpp/base-main.cpp
@@ -6,28 +6,27 @@ typedef vector<int> vi;
 typedef vector<string> vs;
 
 #define FOR(i, a, b) for (int i = (a); i < (b); i++)
-#define rep(i, n)    FOR(i, 0, n)
-#define pb           push_back
-#define all(a)       (a).begin(), (a).end()
-#define UNIQUE(v)    v.erase(unique(all(v)), v.end());
-#define fill(a, x)   memset(a, x, sizeof(a))
-#define zero(a)      fill(a, 0)
-#define minus(a)     fill(a, -1)
-#define dbg(x)       cout << #x "=" << x << endl
+#define rep(i, n) FOR(i, 0, n)
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define UNIQUE(v) v.erase(unique(all(v)), v.end());
+#define fill(a, x) memset(a, x, sizeof(a))
+#define zero(a) fill(a, 0)
+#define minus(a) fill(a, -1)
+#define dbg(x) cout << #x "=" << x << endl
 
 int main() {
-  ios::sync_with_stdio(false);
-  int cntC = 0;
-  vi VCA;
+  int cnt = 0;
+  vi vc;
 
   int n;
   cin >> n;
 
-  while (cntC < n) {
+  while (cnt < n) {
     int tmp;
     cin >> tmp;
-    VCA.pb(tmp);
-    cntC++;
+    vc.pb(tmp);
+    cnt++;
   }
 
   return 0;
