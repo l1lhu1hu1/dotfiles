4e8f273234454a37ed09bc25b7a023d26a1438b0
l1lhu1hu1 <koki827tt@gmail.com>
2026-03-14 15:35:22 +0900

    add: ghosttyの設定ファイルを追加

--- symlink_settings/make_all_symlinks.zsh
+++ symlink_settings/make_all_symlinks.zsh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sh make_vim_symlinks.sh
 sh make_zsh_symlinks.sh
+sh make_ghostty_symlinks.sh
---
+++ symlink_settings/make_ghostty_symlinks.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ln -sf ~/dotfiles/ghostty/config.ghostty \
+  ~/Library/Application\ Support/com.mitchellh.ghostty/config.ghostty
